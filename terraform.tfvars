0d67e1480d2959da640ee08004ff2e7ba751607f
Hafiz Muhammad Umar Rafique <hmurafique.career.93@gmail.com>
2026-06-18 10:28:25 +0000

    Initial commit: AWS Fully Serverless Architecture (Terraform) with GitHub Actions CI/CD for Lambda

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+region             = "us-east-1"
+aws_profile        = "default"
+cidr_block         = "10.0.0.0/16"
+private_subnets    = 2
+database           = "webapp"
+artifact_location  = "./artifact.zip"
+api_stage          = "dev"
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  type = string
+}
+
+variable "aws_profile" {
+  type = string
+}
+
+variable "cidr_block" {
+  type = string
+}
+
+variable "private_subnets" {
+  type = number
+}
+
+variable "database" {
+  type = string
+}
+
+variable "artifact_location" {
+  type = string
+}
+
+
+variable "api_stage" {
+  type = string
+}
